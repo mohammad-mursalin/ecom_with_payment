79bc289333ccfb6a296b17c9dc2d8bd1bdba8238
mohammad-mursalin <mursalinlamon@gmail.com>
2026-04-20 14:55:20 +0600

    Refactor CheckoutSessionResponse to use explicit getters and setters, enhancing code clarity and maintainability

--- backend/ecom/src/main/java/com/mursalin/ecom/dto/CheckoutSessionResponse.java
+++ backend/ecom/src/main/java/com/mursalin/ecom/dto/CheckoutSessionResponse.java
@@ -4,11 +4,44 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class CheckoutSessionResponse {
     private String sessionId;
     private String checkoutUrl;
+
+    public CheckoutSessionResponse(String sessionId, String checkoutUrl, Long orderId) {
+        this.sessionId = sessionId;
+        this.checkoutUrl = checkoutUrl;
+        this.orderId = orderId;
+    }
+
+    public CheckoutSessionResponse() {
+
+    }
+
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(String sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    public String getCheckoutUrl() {
+        return checkoutUrl;
+    }
+
+    public void setCheckoutUrl(String checkoutUrl) {
+        this.checkoutUrl = checkoutUrl;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
     private Long orderId;
+
 }
