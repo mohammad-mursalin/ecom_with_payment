35d16d35487d3798c5c156e0ff15aabed9710c6c
mohammad-mursalin <mursalinlamon@gmail.com>
2026-05-06 23:58:40 +0600

    Update Navbar brand link to point to GitHub profile

--- frontend/ecom-frontend-5-main/src/components/Navbar.jsx
+++ frontend/ecom-frontend-5-main/src/components/Navbar.jsx
@@ -92,8 +92,8 @@ const Navbar = ({ onSelectCategory, onSearch }) => {
       <header>
         <nav className="navbar navbar-expand-lg fixed-top">
           <div className="container-fluid">
-            <a className="navbar-brand" href="https://telusko.com/">
-              Telusko
+            <a className="navbar-brand" href="https://github.com/mohammad-mursalin">
+              Mursalin
             </a>
             <button
               className="navbar-toggler"
