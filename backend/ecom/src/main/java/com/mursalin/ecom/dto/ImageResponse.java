32536937af934f807a4e45ecc65725dba78830b5
mohammad-mursalin <mursalinlamon@gmail.com>
2026-05-06 22:33:26 +0600

    data null bug fixed : used boilerplate getters setters as lombok not working

--- backend/ecom/src/main/java/com/mursalin/ecom/dto/ImageResponse.java
+++ backend/ecom/src/main/java/com/mursalin/ecom/dto/ImageResponse.java
@@ -2,18 +2,62 @@ package com.mursalin.ecom.dto;
 
 import lombok.Data;
 
-@Data
 public class ImageResponse {
 
     private Data data;
     private boolean success;
     private int status;
 
-    @lombok.Data
+    // Getters and Setters for outer class
+    public Data getData() {
+        return data;
+    }
+
+    public void setData(Data data) {
+        this.data = data;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    // Custom helper methods
+    public String getDeleteHash() {
+        return data != null ? data.getDeleteHash() : null;
+    }
+
+    public String getImageUrl() {
+        return data != null ? data.getLink() : null;
+    }
+
+    @Override
+    public String toString() {
+        return "ImageResponse{" +
+                "data=" + data +
+                ", success=" + success +
+                ", status=" + status +
+                '}';
+    }
+
+    // Static inner class
     public static class Data {
+
         private String deletehash;
         private String link;
 
+        // Getters and Setters
         public String getDeleteHash() {
             return deletehash;
         }
@@ -29,13 +73,13 @@ public class ImageResponse {
         public void setLink(String link) {
             this.link = link;
         }
-    }
 
-    public String getDeleteHash() {
-        return data.getDeleteHash();
-    }
-
-    public String getImageUrl() {
-        return data.getLink();
+        @Override
+        public String toString() {
+            return "Data{" +
+                    "deletehash='" + deletehash + '\'' +
+                    ", link='" + link + '\'' +
+                    '}';
+        }
     }
 }
