1a16a8d43932eff309fb2bcf121f155c3be73dc9
mohammad-mursalin <mursalinlamon@gmail.com>
2026-06-16 00:02:57 +0600

    feat: Refactor main.jsx to simplify routing and remove unused context providers

--- frontend/ecom-frontend-5-main/src/main.jsx
+++ frontend/ecom-frontend-5-main/src/main.jsx
@@ -1,18 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import { AppProvider } from "./Context/Context.jsx";
-import { AuthProvider } from "./Context/AuthContext.jsx";
-// import { BrowserRouter as Router } from "react-router-dom";
-ReactDOM.createRoot(document.getElementById("root")).render(
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <Router> */}
-      <AuthProvider>
-        <AppProvider>
-          <App />
-        </AppProvider>
-      </AuthProvider>
-    {/* </Router> */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
-);
+)
