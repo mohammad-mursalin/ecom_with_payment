7bcc9d8af8b51acabdb4a1bfd5d8e79c183bc4ee
mohammad-mursalin <mursalinlamon@gmail.com>
2026-06-21 12:07:58 +0600

    refactor: Replace @NoArgsConstructor with @RequiredArgsConstructor in ErrorResponse class

--- backend/ecom/src/main/java/com/mursalin/ecom/dto/ErrorResponse.java
+++ backend/ecom/src/main/java/com/mursalin/ecom/dto/ErrorResponse.java
@@ -3,15 +3,21 @@ package com.mursalin.ecom.dto;
 import java.time.Instant;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
+@RequiredArgsConstructor
 public class ErrorResponse {
 
     private int status;
     private String error;
     private String message;
     private Instant timestamp;
+
+    public ErrorResponse(int status, String error, String message) {
+        this.status = status;
+        this.error = error;
+        this.message = message;
+    }
 }
