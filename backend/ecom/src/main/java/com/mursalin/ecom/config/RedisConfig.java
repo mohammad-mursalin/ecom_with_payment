6b784d93ac2be4bf25f6d5310c3c9bbcc248d881
mohammad-mursalin <mursalinlamon@gmail.com>
2026-07-19 02:23:21 +0600

    refactor: update ObjectMapper configuration in RedisConfig for improved date handling

--- backend/ecom/src/main/java/com/mursalin/ecom/config/RedisConfig.java
+++ backend/ecom/src/main/java/com/mursalin/ecom/config/RedisConfig.java
@@ -2,7 +2,9 @@ package com.mursalin.ecom.config;
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -36,9 +38,10 @@ public class RedisConfig {
 			.build();
 	}
 
-	@Bean
-	public ObjectMapper objectMapper() {
+	private ObjectMapper objectMapper() {
 		ObjectMapper mapper = new ObjectMapper();
+		mapper.registerModule(new JavaTimeModule());
+		mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 		mapper.activateDefaultTyping(
 			LaissezFaireSubTypeValidator.instance,
 			ObjectMapper.DefaultTyping.NON_FINAL,
